b3863343c387c96283cac70eb4388064afad61f3
liujiajie <liujiajie@dianping.com>
2017-03-08 15:28:34 +0800

    【更新JJFLEXToolbarItem】

--- hitTestPractice/hitTestPractice/JJFLEX/JJFLEXToolbarItem.m
+++ hitTestPractice/hitTestPractice/JJFLEX/JJFLEXToolbarItem.m
@@ -8,14 +8,40 @@
 
 #import "JJFLEXToolbarItem.h"
 
+@interface JJFLEXToolbarItem ()
+
+@end
+
 @implementation JJFLEXToolbarItem
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (id)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.backgroundColor = [[self class] defaultBackgroundColor];
+        [self setTitleColor:[[self class] defaultTitleColor] forState:UIControlStateNormal];
+        [self setTitleColor:[[self class] disabledTitleColor] forState:UIControlStateDisabled];
+    }
+    return self;
 }
-*/
+
++ (UIColor *)defaultBackgroundColor {
+    return [UIColor colorWithWhite:1.0 alpha:0.95];
+}
+
++ (UIColor *)highlightedBackgroundColor {
+    return [UIColor colorWithWhite:0.9 alpha:1.0];
+}
+
++ (UIColor *)selectedBackgroundColor {
+    return [UIColor colorWithRed:199.0/255.0 green:199.0/255.0 blue:255.0/255.0 alpha:1.0];
+}
+
++ (UIColor *)defaultTitleColor {
+    return [UIColor blackColor];
+}
+
++ (UIColor *)disabledTitleColor {
+    return [UIColor colorWithWhite:121.0/255.0 alpha:1.0];
+}
 
 @end
